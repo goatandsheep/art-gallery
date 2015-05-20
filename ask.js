2196b8420fb474e46818aedffb5eb820c595f48c
goatandsheep <goatandsheep@gmail.com>
2015-05-20 15:14:41 -0400

    gitignore, put external script to store questions

---
+++ ask.js
@@ -0,0 +1,16 @@
+var question = [][];
+
+question[0][0] = "How do you think the boy is feeling?";
+question[0][1] = "sad";
+question[0][2] = "happy";
+question[0][3] = "confused";
+
+question[1][0] = "What do you think he is pointing at?";
+question[1][1] = "lake";
+question[1][2] = "duck";
+question[1][3] = "pigeon";
+
+question[2][0] = "What kind of gun is in his hand?";
+question[2][1] = "rifle";
+question[2][2] = "shotgun";
+question[2][3] = "revolver";
