8fe155b7b9e0d65a5583a43feef5787dbb1f88a3
goatandsheep <goatandsheep@gmail.com>
2015-05-21 14:58:10 -0400

    i think we're done

--- ask.js
+++ ask.js
@@ -1,66 +1,65 @@
-var question = []
-
-question[0] = [];
-question[0][0] = true;
-question[0][1] = "How do you think the boy is feeling?";
-question[0][2] = "sad";
-question[0][3] = "happy";
-question[0][4] = "confused";
-
-question[1] = [];
-question[1][0] = true;
-question[1][1] = "What do you think he is pointing at?";
-question[1][2] = "lake";
-question[1][3] = "duck";
-question[1][4] = "pigeon";
-
-question[2] = [];
-question[2][0] = true;
-question[2][1] = "What kind of gun is in his hand?";
-question[2][2] = "rifle";
-question[2][3] = "shotgun";
-question[2][4] = "revolver";
-
-var remaining = question.length;
+var question = [];
+var remaining = 0;
 
 function start(){
-	document.getElementByID('question').innerHTML = "Feel free to review the highlighted parts of the art.";
+
+	question[0] = [];
+	question[0][0] = true;
+	question[0][1] = "How do you think the boy is feeling?";
+	question[0][2] = "sad";
+	question[0][3] = "happy";
+	question[0][4] = "confused";
+
+	question[1] = [];
+	question[1][0] = true;
+	question[1][1] = "What do you think he is pointing at?";
+	question[1][2] = "lake";
+	question[1][3] = "duck";
+	question[1][4] = "pigeon";
+
+	question[2] = [];
+	question[2][0] = true;
+	question[2][1] = "What kind of gun is in his hand?";
+	question[2][2] = "rifle";
+	question[2][3] = "shotgun";
+	question[2][4] = "revolver";
+
+	remaining = question.length;
+	document.getElementById('question').innerHTML = "Feel free to review the highlighted parts of the art.";
+	//load image locations
+	for (i = 0; i < question.length; i++){
+		document.getElementById('column-mid').innerHTML += "<button class='bob' id='section" + i + "' onclick='questInsert("+ i +");'></button>";
+	}
 }
 
 function questInsert(qNum){
+
 	if (question[qNum][0]){
-		document.getElementByID('question').innerHTML = question[qNum][1];
+		document.getElementById('question').innerHTML = question[qNum][1];
 		//TODO: change class of answer bubbles to visible
 		//TODO: change answer bubbles to qNum
 		//TODO: change contents of answer bubbles
-
 		for (i = 2; i < question[qNum].length; i++){
-			/*
-			bubble[i].innerHTML = question[qNum][i];
-			bubble[i].className = 'visible';
-			bubble
-			*/
+			document.getElementById('options').innerHTML += ("<button class='answers' onclick='selectAnswer(" + qNum + ", " + i + ")'>" + question[qNum][i] + "</button>");
 		}
 	}
 }
 
 function selectAnswer(qNum, aNum){
-	+="<tr><td>" + "</td><td>" +"</td></tr>"
+	document.getElementById('answers').innerHTML += ("<tr><td>" + "<div id='bubble" + qNum + "' class='imgBubble'></div>" + "</td><td>" + question[qNum][aNum] + "</td></tr>");
 
-	//TODO: change class of answer bubbles to hidden
-	for (i = 2; i < question[qNum].length; i++){
-		question[qNum][i].className = 'hidden';
-	}
+	//clear the options	
+	document.getElementById('options').innerHTML = " ";
 	question[qNum][0] = false;
 	remaining = remaining - 1;
-
-	if (remaining != 0){
-		document.getElementByID('question').innerHTML = "Select another area of the art to explore.";
+	if (remaining > 0){
+		document.getElementById('question').innerHTML = "Select another area of the art to explore.";
 	}
 	else{
-		document.getElementByID('question').innerHTML = "Thank you for exploring the art on Art on the Brain.";
+		document.getElementById('question').innerHTML = "Thank you for exploring the art on Art on the Brain.";
 	}
 }
+
 /*
 function addPoint(){
 	question[question.length] = []; //increases the length of question
